674667442b4d2e4bc188df96d815d86ba3cc20fc
Gayashan Sameera <gayashansameera30@gmail.com>
2018-11-17 18:13:15 +0530

    react router

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers'
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './history';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const middleware = [thunk];
 const createStoreWithMddleware = applyMiddleware(...middleware)(createStore);
-const store =createStoreWithMddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSIONS__&&window.__REDUX_DEVTOOLS_EXTENSIONS__() )
+const store =createStoreWithMddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+       <Router history={history}>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/ab" component={App}>
+                    <Route path="/app" component={App}/>
+                </Route>
+            </Switch>
+        </Router>
     </Provider>, 
     document.getElementById('root'));
 serviceWorker.unregister();
